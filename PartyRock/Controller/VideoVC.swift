d264e8dd96ab60bcc4dfc4c9851be6fcfef6f30f
Sujanth <sujanth1987@gmail.com>
2017-08-31 21:27:47 +1000

    Detail view controller added

---
+++ PartyRock/Controller/VideoVC.swift
@@ -0,0 +1,34 @@
+//
+//  VideoVC.swift
+//  PartyRock
+//
+//  Created by Sujanth Sebamalaithasan on 31/8/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+
+class VideoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
