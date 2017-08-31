239809f3cf084e03c3b544fd82b8551553ee9f29
Sujanth <sujanth1987@gmail.com>
2017-08-31 20:53:31 +1000

    Project started

---
+++ PartyRock/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PartyRock
+//
+//  Created by Sujanth Sebamalaithasan on 31/8/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+   
+
+
+}
+
