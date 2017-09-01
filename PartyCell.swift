932fff073622ef8e875285c247c19ac060462a78
Sujanth <sujanth1987@gmail.com>
2017-09-01 15:06:56 +1000

    Party Cell updated

--- PartyCell.swift
+++ PartyCell.swift
@@ -18,11 +18,10 @@ class PartyCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    
+    func updateUI(partRock: PartyRock) {
+        videoTitle.text = partRock.videoTitle
     }
 
 }
